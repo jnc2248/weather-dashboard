3909526faf8260b2f13cbca7d2e18c98d16794fa
[j chun] <[justinchun224@gmail.com]>
2023-09-28 17:18:21 -0700

    added search bar and function to get search city's coordinates for weather api

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,23 @@ searchBar.addEventListener("submit", function(event) {
 
     var citySearch = event.target.citySearch.value;
     console.log(citySearch, 'Success');
+
+    getCoordinates(citySearch);
 });
 
-function getCoordinates() {
-    var URL = 'http://api.openweathermap.org/geo/1.0/direct?q=' +  + '&limit=5&appid=885e7a2453d9981f57f30a861b8634ec'
+function getCoordinates(city) {
+    var URL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=885e7a2453d9981f57f30a861b8634ec'
+
+    fetch(URL).then(function(data) {
+        return data.json();
+    }).then(function(data) {
+        console.log(data, data[0].lat, data[0].lon);
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        searchCity(latitude, longitude);
+    })
 };
 
-function searchCity() {
+function searchCity(latitude, longitude) {
     var URL = ''
 };
