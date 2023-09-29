78ee9583c3e77ea798a54a0e96671c6197de0907
[j chun] <[justinchun224@gmail.com]>
2023-09-28 19:57:05 -0700

    added 5 day forecast info

--- assets/script.js
+++ assets/script.js
@@ -37,10 +37,12 @@ function searchCity(latitude, longitude) {
         var feels = data.main.feels_like;
         var tempMax = data.main.temp_max;
         var tempMin = data.main.temp_min;
+        var wind = data.wind.speed;
         var humidity = data.main.humidity;
 
         var feelsText = document.createElement("div");
         var tempMaxMinText = document.createElement("div");
+        var windText = document.createElement("div");
         var humidityText = document.createElement("div");
         var mainTemp = document.createElement("div");
         var space = document.createElement("br");
@@ -50,17 +52,16 @@ function searchCity(latitude, longitude) {
         mainTemp.innerHTML = Math.trunc((temp - 273.15) * (9/5) + 32) + "°F";
         feelsText.innerHTML = "Feels like " + Math.trunc((feels - 273.15) * (9/5) + 32) + "°F";
         tempMaxMinText.innerHTML = "Max " + Math.trunc((tempMax - 273.15) * (9/5) + 32) + "°F / Min " + Math.trunc((tempMin - 273.15) * (9/5) + 32) + "°F";
-        humidityText.innerHTML = "Humidity " + humidity + "%";
+        windText.innerHTML = "Wind: " + wind + " mph";
+        humidityText.innerHTML = "Humidity: " + humidity + "%";
 
-        // Display time in searched city
-        var format = dayjs().format('MMM D, YYYY h:mm a');
-        console.log(format);
+        var format = dayjs().format('MMM D, YYYY');
 
         $("#currentInfo").append(name, space);
         $("#currentInfo").append(format);
         $("#currentInfo").append('<img src="" id="icon" alt="Weather Icon">', description);
         $("#currentInfo").append(mainTemp);
-        $("#moreInfo").append(feelsText, tempMaxMinText, humidityText);
+        $("#moreInfo").append(feelsText, tempMaxMinText, windText, humidityText);
 
         var iconURL = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
         document.getElementById("icon").setAttribute("src", iconURL);
@@ -74,5 +75,36 @@ function fiveDayForecast(latitude, longitude) {
         return data.json();
     }).then(function(data) {
         console.log(data);
+
+        var today = dayjs();
+        // var tomorrow = today.add(1, 'day');
+        // var formatted = tomorrow.format('MMM D, YYYY');
+        // console.log(formatted);
+
+        var displayLength = 6
+        for (i = 1; i < displayLength; i++) {
+            var date = today.add(i, 'day');
+            var formatted = date.format('MMM D, YYYY');
+            var temp = data.list[i].main.temp;
+            var wind = data.list[i].wind.speed;
+            var humidity = data.list[i].main.humidity;
+
+            var tempText = document.createElement("div");
+            var windText = document.createElement("div");
+            var humidityText = document.createElement("div");
+            var dateText = document.createElement("div");
+            var space = document.createElement("br");
+
+            tempText.innerHTML = Math.trunc((temp - 273.15) * (9/5) + 32) + "°F";
+            windText.innerHTML = "Wind: " + wind + " mph";
+            humidityText.innerHTML = "Humidity: " + humidity + "%";
+            dateText.innerHTML = formatted;
+
+            var oneDay = document.createElement("div");
+
+            oneDay.append(dateText, tempText, windText, humidityText, space, space);
+
+            $("#fiveDay").append(oneDay);
+        };
     })
 }
